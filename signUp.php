4a27bf539d1a5c2a6b124d9a7d7855d3b166ea58
DaviesHuang <yh6714@ic.ac.uk>
2016-06-04 22:15:55 +0100

    signUp php script added

---
+++ signUp.php
@@ -0,0 +1,9 @@
+<?php
+    //$db_connection = pg_connect("host=db.doc.ic.ac.uk dbname=g1527135_u user=g1527135_u password=group35");
+    
+    //$result = pg_query($db_connection, "insert into lol values(13,'2','34');");
+     
+   echo $_POST["username"];  
+   echo '<br/-->';
+
+?>
